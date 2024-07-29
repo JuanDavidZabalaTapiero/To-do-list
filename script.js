338a31e70fcf644898df8b7ca10d243b3b8f87f2
JuanDavidZabalaTapiero <juandavidzabalatapiero@gmail.com>
2024-07-29 11:28:30 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+
+var list_tareas = document.getElementById("list_tareas");
+
+// EVENTO AL ADD TAREA
+const add_tarea = document.getElementById("add_tarea");
+
+add_tarea.addEventListener('click', function () {
+    var input_tarea = document.getElementById("input_tarea");
+
+    var div_tarea = document.createElement("div");
+    div_tarea.className = 'div_tarea';
+
+    var name_tarea = document.createElement("p");
+    name_tarea.textContent = input_tarea.value;
+
+    var button_delete = document.createElement("button");
+    button_delete.textContent = "D";
+
+    // AÑADO EL DIV ADENTRO DE LA LISTA
+    list_tareas.appendChild(div_tarea);
+
+    // AÑADO LA TAREA AL DIV
+    div_tarea.appendChild(name_tarea);
+
+    // AÑADO EL BTN DE DELETE AL DIV
+    div_tarea.appendChild(button_delete);
+
+    // AÑADO EVENTO DE CLICK AL BTN DE DELETE
+    button_delete.addEventListener('click', function () {
+        list_tareas.removeChild(div_tarea);
+    });
+})

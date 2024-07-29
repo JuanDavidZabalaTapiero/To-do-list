e17a39007fc00a2630a278298a4a604c726cdadb
JuanDavidZabalaTapiero <juandavidzabalatapiero@gmail.com>
2024-07-29 12:16:54 -0500

    Añadí el estilo de la tarea

--- src/js/script.js
+++ src/js/script.js
@@ -14,7 +14,13 @@ add_tarea.addEventListener('click', function () {
     name_tarea.textContent = input_tarea.value;
 
     var button_delete = document.createElement("button");
-    button_delete.textContent = "D";
+
+    var icono_borrar = document.createElement("i");
+    icono_borrar.className = "fa-solid fa-trash";
+    icono_borrar.style.color = "#ffffff";
+
+    // AÑADO EL ICONO AL BTN
+    button_delete.appendChild(icono_borrar);
 
     // AÑADO EL DIV ADENTRO DE LA LISTA
     list_tareas.appendChild(div_tarea);
@@ -29,4 +35,7 @@ add_tarea.addEventListener('click', function () {
     button_delete.addEventListener('click', function () {
         list_tareas.removeChild(div_tarea);
     });
+
+    // DEJO EL INPUT LIMPIO
+    input_tarea.value = '';
 })
